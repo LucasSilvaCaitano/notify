d85a4fda443c250c4cebe04fc667092daf96cae7
LucasSilvaCaitano <lucas.caitano@bandtec.com.br>
2022-10-04 08:10:20 -0300

    Adicionando repositorio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import './firebase-messaging-sw'
+import Notification from './notification/Notification'
+
+function App() {
+  return (
+    <>
+      <h1>Oi</h1>
+      <Notification />
+    </>
+  );
+}
+
+export default App;
